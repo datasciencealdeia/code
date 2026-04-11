0775748eda2d7b790dacb25276beb8fb15e9539f
datasciencealdeia <datascienceandbigdata@gmail.com>
2026-04-10 21:07:29 -0300

    merge

--- ufpr/genai/pyai/api/consumidor.py
+++ ufpr/genai/pyai/api/consumidor.py
@@ -9,21 +9,11 @@ geo_params = {
    "count": 1
 }
 geo_response = requests.get(geo_url, params=geo_params)
-geo_data = geo_response.json()
-
-lat = geo_data["results"][0]["latitude"]
-lon = geo_data["results"][0]["longitude"]
-
-print("Lat:", lat, "Lon:", lon)
-
-weather_url = "https://api.open-meteo.com/v1/forecast"
-
 weather_params = {
    "latitude": lat,
    "longitude": lon,
    "current_weather": True
 }
 weather_response = requests.get(weather_url, params=weather_params)
-weather_data = weather_response.json()
 
 print("Temperatura:", weather_data["current_weather"]["temperature"])
